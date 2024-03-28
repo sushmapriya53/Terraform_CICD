b43c71a2feff9fb63e9f4339d7164999e2840f22
sushmapriya53 <162276888+sushmapriya53@users.noreply.github.com>
2024-03-28 09:59:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-02d7fd1c2af6eead0"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name      =  "serverkey"
     tags = {
       Name = "dev-ec2"
